a9f65fdc528b1d9077f67b980eba36a2c39ccd5f
Kacper Wierzba <s30508@pjwstk.edu.pl>
2025-03-21 12:55:18 +0100

    added generic class for conteners to inherit from

---
+++ Kontener.cs
@@ -0,0 +1,36 @@
+﻿namespace APBD3;
+
+public abstract class Kontener
+{
+    public float MassWithCargo{set; get;}
+    public float MassOfContener{set; get;}
+    public float Height{set; get;}
+    public float Depth{set; get;}
+    public float MaxVolumeInKg{set; get;}
+
+    protected string SerialNumber
+    {
+        set
+        {
+            SerialNumber = "KON-"+value+"-"+Kontener.nextNum;
+        }
+        get
+        {
+            return SerialNumber;
+        }
+    }
+    private static int nextNum
+    {
+        set
+        {
+            throw new Exception("cant change automated filed");
+        }
+        get
+        {
+            return nextNum++;
+        }
+    }
+    
+    
+
+}
